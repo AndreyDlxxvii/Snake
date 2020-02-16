bb5967498046a0ac6d6e35c6f4c0da59519ac074
AndreyDlxxvii <dlxxvii@gmail.com>
2020-02-16 13:57:39 +0300

    Add first game

---
+++ Assets/MoveSnake.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class MoveSnake : MonoBehaviour
+{
+    public float speed;
+    public GameObject body;
+    public Vector2 direction;
+    public Text textUI;
+
+    private bool flag;
+    private List<Transform> posTailTrans = new List<Transform>();
+    private Vector2 tempPos;
+    private GameObject child;
+    private int score = 0;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        flag = false;
+        var temp = collision.collider.tag;
+        switch (temp)
+        {
+            case "vertical":
+                var i = transform.rotation.eulerAngles.z;
+                transform.rotation = Quaternion.Euler(0, 0, -i);
+                break;
+            case "up":
+                transform.rotation = Quaternion.Euler(0, 0, -180);
+                break;
+            case "down":
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                break;
+            case "food":
+                score++;
+                flag = true;
+                break;
+        }
+        textUI.text = "Score" + " " + score.ToString();
+    }
+    void Start()
+    {
+        InvokeRepeating("Move", 0, 0.12f);
+        
+        //EatFood();
+
+    }
+
+    private void Move()
+    {
+        tempPos = transform.position;
+        transform.Translate(direction);
+        if (Input.GetKey(KeyCode.W) && transform.rotation.eulerAngles.z != 0)
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        if (Input.GetKey(KeyCode.S) && transform.rotation.eulerAngles.z != -180)
+            transform.rotation = Quaternion.Euler(0, 0, -180);
+        if (Input.GetKey(KeyCode.A) && transform.rotation.eulerAngles.z != 90)
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        if (Input.GetKey(KeyCode.D) && transform.rotation.eulerAngles.z != -90)
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+
+        if (flag == true)
+        {
+            GameObject tail = Instantiate(body, tempPos, Quaternion.identity);
+            posTailTrans.Insert(0, tail.transform);
+            flag = false;
+
+
+        }
+        else if (posTailTrans.Count > 0)
+        {
+            posTailTrans.Last().position = tempPos;
+            posTailTrans.Insert(0, posTailTrans.Last());
+            posTailTrans.RemoveAt(posTailTrans.Count - 1);
+        }
+    }
+}
---
+++ Assets/SpawnFood.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    public GameObject foodPrefab;
+
+    public Transform vertical_left;
+    public Transform vertical_right;
+    public Transform horisontal_up;
+    public Transform horisontal_down;
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0, 10);
+    }
+
+   void Spawn ()
+    {
+        var x = Random.Range(vertical_left.position.x, vertical_right.position.x);
+        var y = Random.Range(horisontal_down.position.y, horisontal_up.position.y);
+        Vector2 vector = new Vector2(x, y);
+        GameObject temp = Instantiate(foodPrefab, vector, Quaternion.identity);
+    }
+
+
+}
